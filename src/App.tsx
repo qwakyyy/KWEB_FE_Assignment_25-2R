24ae5bc7cffb42b433fa88e89a2e360e663ab295
Qwakyyy <malditojmin@gmail.com>
2025-11-30 00:04:58 +0900

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react'
+import appStyle from './app.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoList, setTodoList] = useState<string[]>([])
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  function Add() {
+    if (inputRef.current) {
+      setTodoList([...todoList, inputRef.current.value])
+      inputRef.current.value = ""
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={appStyle.container}>
+      <h1>Todo List</h1>
+      {
+        todoList.map((todo, index) => (
+          <Item key={index} todo={todo} index={index} remove={
+            () => setTodoList(todoList.filter((_, i) => i !== index))
+          }></Item>
+      ))}
+      <div className="input">
+        <input type='text' ref={inputRef}></input>
+        <button onClick={Add}>+</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  )
+}
+
+type ItemProps = {
+  todo: string,
+  index: number,
+  remove: () => void
+}
+
+function Item({ todo, remove } : ItemProps) {
+  return (
+    <div>
+      <span className={appStyle.todo}>{todo}</span>
+        <button onClick={remove}>X</button>
+    </div>
   )
 }
 
